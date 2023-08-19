b53540c4caa19a21de464da0cd40ab25f1b0e1a5
ringhint <47498421+ringhint@users.noreply.github.com>
2023-08-19 23:04:55 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-set -e
-
-if [ ! -f index.php ]; then
-    git clone --depth 1 -q ${REPO_URL} .
-    rm -rf .git*
-    chmod a+rw -R application runtime upload static addons
-    echo "maccms10 downloaded"
-fi
-
-if [ -f admin.php ]; then
-    mv admin.php $ADMIN_PHP
-    echo "admin.php => $ADMIN_PHP"
-fi
-
-exec "$@"
+curl -sL https://github.com/aoaostar/toolbox/raw/main/docker/install.sh | bash docker-composer up -d
