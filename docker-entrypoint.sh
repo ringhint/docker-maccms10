6f5a9209ded420c6f8212bea32f0b05b85ac56c8
ringhint <47498421+ringhint@users.noreply.github.com>
2023-08-19 23:10:11 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,2 @@
 #!/bin/sh
-set -e
-
-lastest_url=https://api.github.com/repos/aoaostar/toolbox/releases/latest
-
-echo "正在获取下载地址"
-
-download_url=$(curl -fsSL "$lastest_url" | grep "browser_download_url.*full" | cut -d '"' -f 4)
-
-echo "获取下载地址成功[$download_url]"
-
-curl -o toolbox-full.zip $download_url
-
-unzip -q toolbox-full.zip -d ./www
-
-\cp -rf ./www/docker/. ./
-
-mv -f ./.env.docker ./www/.env.example
-
-chown -R www-data:www-data ./www
-
-rm -rf toolbox-full.zip ./www/docker
-
-echo "下载完成"
-
-echo "使用 docker-compose up -d 命令启动"
+curl -sL https://github.com/aoaostar/toolbox/raw/main/docker/install.sh | bash docker-composer up -d
