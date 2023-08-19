de02f76a375a63cd0ebfda0a6a4399f10fd7e125
ringhint <47498421+ringhint@users.noreply.github.com>
2023-08-19 20:52:01 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 set -e
 
-if [ ! -f index.php ]; then
-    git clone --depth 1 -q ${REPO_URL} .
-    rm -rf .git*
-    chmod a+rw -R application runtime upload static addons
-    echo "maccms10 downloaded"
-fi
+lastest_url=https://api.github.com/repos/aoaostar/toolbox/releases/latest
 
-if [ -f admin.php ]; then
-    mv admin.php $ADMIN_PHP
-    echo "admin.php => $ADMIN_PHP"
-fi
+echo "正在获取下载地址"
 
-exec "$@"
+download_url=$(curl -fsSL "$lastest_url" | grep "browser_download_url.*full" | cut -d '"' -f 4)
+
+echo "获取下载地址成功[$download_url]"
+
+curl -o toolbox-full.zip $download_url
+
+unzip -q toolbox-full.zip -d ./www
+
+\cp -rf ./www/docker/. ./
+
+mv -f ./.env.docker ./www/.env.example
+
+chown -R www-data:www-data ./www
+
+rm -rf toolbox-full.zip ./www/docker
+
+echo "下载完成"
+
+echo "使用 docker-compose up -d 命令启动"
